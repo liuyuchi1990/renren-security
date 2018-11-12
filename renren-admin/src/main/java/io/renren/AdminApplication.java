2916627f202d729c8ed1a311a8d0b4a50c10dbe2
richard <richard.liu@kpmg.com>
2018-11-13 00:26:51 +0800

    上传git提交ignore文件

--- renren-admin/src/main/java/io/renren/AdminApplication.java
+++ renren-admin/src/main/java/io/renren/AdminApplication.java
@@ -1,10 +1,13 @@
 package io.renren;
 
+import io.renren.modules.sys.controller.OriginFilter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -18,5 +21,16 @@ public class AdminApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(AdminApplication.class);
+	}
+
+	@Bean
+	public FilterRegistrationBean testFilterRegistration() {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(new OriginFilter());
+		registration.addUrlPatterns("/*");
+		registration.addInitParameter("user_id", "123");
+		registration.setName("originFilter");
+		registration.setOrder(1);
+		return registration;
 	}
 }
