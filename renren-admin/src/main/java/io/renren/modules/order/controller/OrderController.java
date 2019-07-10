bc89870c01e489b7829a8d3931126a71f9bbb2bb
richard <richard.liu@kpmg.com>
2019-07-10 17:48:06 +0800

    新增集赞模块

--- renren-admin/src/main/java/io/renren/modules/order/controller/OrderController.java
+++ renren-admin/src/main/java/io/renren/modules/order/controller/OrderController.java
@@ -2,7 +2,7 @@ package io.renren.modules.order.controller;
 
 import com.alibaba.fastjson.JSON;
 import io.renren.common.config.Constants;
-import io.renren.common.utils.Constant;
+
 import io.renren.modules.distribution.entity.Distribution;
 import io.renren.modules.distribution.service.DistributionService;
 import io.renren.modules.order.model.Order;
@@ -32,8 +32,6 @@ import java.util.concurrent.Executors;
 @RequestMapping("/api/order")
 public class OrderController {
 
-    //    @Autowired
-//    BreadcrumbUtil breadcrumbUtil;
     @Autowired
     OrderService orderService;
 
@@ -42,55 +40,6 @@ public class OrderController {
 
     @Autowired
     DistributionService distributionService;
-//    @Autowired
-//    SysUserService sysUserService;
-//    @Autowired
-//    DeviceService deviceService;
-//    @Autowired
-//    PageUtil pageUtil;
-//
-//    @Autowired
-//    ThreadPoolManager tpm;
-//
-//    @ApiIgnore
-//    @RequestMapping("/getListPage")
-//    @RequiresPermissions(value = {"order:query"})
-//    public String list() {
-//        List<Breadcrumb> Breadcrumbs = breadcrumbUtil.getBreadcrumbPath("order/getListPage");
-//        return JSON.toJSONString(Breadcrumbs);
-//    }
-//
-//    @ApiIgnore
-//    @RequestMapping("/addPage")
-//    @RequiresPermissions(value = {"order:query"})
-//    public String addPage() {
-//        List<Breadcrumb> Breadcrumbs = breadcrumbUtil.getBreadcrumbPath("order/addPage");
-//        return JSON.toJSONString(Breadcrumbs);
-//    }
-//
-//    @ApiIgnore
-//    @RequestMapping("/editPage")
-//    @RequiresPermissions(value = {"order:query"})
-//    public String editPage() {
-//        List<Breadcrumb> Breadcrumbs = breadcrumbUtil.getBreadcrumbPath("order/editPage");
-//        return JSON.toJSONString(Breadcrumbs);
-//    }
-//
-//
-//    @ApiIgnore
-//    @RequestMapping(value = "/queryPage", method = RequestMethod.GET)
-//    @RequiresPermissions(value = {"order:query"})
-//    public String queryPage(
-//            @RequestParam(required = false) String user_name,
-//            @RequestParam(required = false) String begindate,
-//            @RequestParam(required = false) String enddate,
-//            @RequestParam(required = false) String order_status,
-//
-//            @RequestParam int pageNumber, @RequestParam int pageSize) {
-//        TablePage tp = pageUtil.getDataForPaging(orderService.queryPage(user_name, DateUtil.StrtoDate(begindate, "yyyy-MM-dd"), DateUtil.StrtoDate(enddate, "yyyy-MM-dd"), order_status, pageNumber, pageSize));
-//        return JSON.toJSONString(tp);
-//    }
-//
 
     /**
      * 订单生成
@@ -185,7 +134,7 @@ public class OrderController {
         Map<String, Object> map = new HashMap<>();
         order.setOrderStatus("1");
         order.setOrderId(UUID.randomUUID().toString().replaceAll("-", ""));
-        if(Constants.GROUPON.equals(order.getOrderType())&&(order.getGroupId()!=null)&&(!"".equals(order.getGroupId()))){
+        if (Constants.GROUPON.equals(order.getOrderType()) && (order.getGroupId() != null) && (!"".equals(order.getGroupId()))) {
             order.setGroupId(UUID.randomUUID().toString().replaceAll("-", ""));
         }
         user.setUserId(order.getUser_id());
@@ -206,116 +155,6 @@ public class OrderController {
         return result;
     }
 
-    //
-//    @RequestMapping(value = "/delete", method = RequestMethod.GET)
-//    @ResponseBody
-//    @Transactional(rollbackFor = Exception.class)
-//    public ReturnResult delete(@RequestParam String ids) {
-//        ReturnResult result = new ReturnResult(ReturnCodeEnum.SUCCESS.getCode(), ReturnCodeEnum.SUCCESS.getMessage());
-//        Map<String, Object> map = new HashMap<>();
-//        Order order = orderService.queryById(ids);
-//        try {
-//            orderService.delete(ids.replaceAll("\"", ""));
-//            deviceService.rollback(order);
-//            map.put("status", "成功");
-//            return result;
-//        } catch (Exception e) {
-//            result.setCode(ReturnCodeEnum.SYSTEM_ERROR.getCode());
-//            result.setMsg(ReturnCodeEnum.SYSTEM_ERROR.getMessage());
-//            map.put("status", "失败");
-//            return result;
-//        }
-//    }
-//
-//    @RequestMapping(value = "/getAllOrder", method = RequestMethod.GET)
-//    @Transactional(isolation = Isolation.READ_COMMITTED)
-//    public ReturnResult getAllOrder(@RequestParam(required = true) String device_id,
-//                                    @RequestParam(required = false) String networkStatus,
-//                                    @RequestParam(required = false) String temperature) {
-//        ReturnResult result = new ReturnResult(ReturnCodeEnum.SUCCESS.getCode(), ReturnCodeEnum.SUCCESS.getMessage());
-//        Map<String, Object> map = new HashMap<>();
-//        map = orderService.queryByDeviceId(device_id);
-//        Date day = new Date();
-//        SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
-//        if(map==null){
-//            map = new HashMap<>();
-//            map.put("order_id","");
-//            map.put("cargo_lane","");
-//        }
-//        map.put("timeStamp", df.format(day));
-//        result.setResult(map);
-//        return result;
-//    }
-//
-////    @ApiOperation(value = "获取掉货信息", response = OrderInfo.class)
-////    @Transactional(rollbackFor = {Exception.class}, readOnly = false)
-////    @RequestMapping(value = "/getOrderFeedBack", method = RequestMethod.POST)
-////    @ResponseBody
-////    public ReturnResult getOrderFeedBack(@ApiParam @RequestBody OrderInfo orderInfo) {
-////        ReturnResult result = new ReturnResult(ReturnCodeEnum.SUCCESS.getCode(), ReturnCodeEnum.SUCCESS.getMessage());
-////        Map<String, Object> map = new HashMap<>();
-////        Integer count = 0;
-////        List<Cargo> CargoLst = Arrays.asList(orderInfo.getCargo_lane());
-////        for (Cargo f : CargoLst) {
-////            if (("true".equals(f.getStatus()))) {
-////                count++;
-////            }
-////        }
-////        if (count != CargoLst.size()) {
-////            orderInfo.setOrder_status("2");
-////        } else {
-////            orderInfo.setOrder_status("4");
-////        }
-////        orderInfo.setIntegral(count * Constants.Integral);
-////
-////        try{
-////            int rs = orderService.edit(orderInfo);
-////            int rs2 = orderService.updateUserIntegral(orderInfo);
-////            map.put("status", "成功");
-////            result.setResult(map);
-////        } catch (Exception e){
-////            result.setCode(ReturnCodeEnum.SYSTEM_ERROR.getCode());
-////            result.setMsg(ReturnCodeEnum.SYSTEM_ERROR.getMessage());
-////            if (count != CargoLst.size()) {
-////                map.put("status", "掉货失败");
-////            } else {
-////                map.put("status", "失败");
-////            }
-////            result.setResult(map);
-////        }
-////        return result;
-////    }
-//
-//    @RequestMapping(value = "/getOrderPay", method = RequestMethod.POST)
-//    @ResponseBody
-//    public ReturnResult getOrderPay(@ApiParam @RequestBody OrderInfo orderInfo) {
-//        ReturnResult result = new ReturnResult(ReturnCodeEnum.SUCCESS.getCode(), ReturnCodeEnum.SUCCESS.getMessage());
-//        Map<String, Object> map = new HashMap<>();
-//        orderInfo.setOrder_status("3");
-//        orderInfo.setPay_type("1");
-//        Map user = sysUserService.queryById(orderInfo.getUser_id());
-//        if (Integer.parseInt(user.get("integral").toString()) > orderInfo.getIntegral()) {
-//            try {
-//                int rs = orderService.edit(orderInfo);
-//                int rs2 = orderService.minusUserIntegral(orderInfo);
-//                map.put("status", "成功");
-//                result.setResult(map);
-//            } catch (Exception e) {
-//                result.setCode(ReturnCodeEnum.SYSTEM_ERROR.getCode());
-//                result.setMsg(ReturnCodeEnum.SYSTEM_ERROR.getMessage());
-//                map.put("status", "失败");
-//                result.setResult(map);
-//            }
-//
-//        } else {
-//            result.setCode(ReturnCodeEnum.SYSTEM_ERROR.getCode());
-//            result.setMsg(ReturnCodeEnum.SYSTEM_ERROR.getMessage());
-//            map.put("status", "积分不够");
-//            result.setResult(map);
-//        }
-//        return result;
-//    }
-//
     @RequestMapping(value = "/getOrderByFromUserId", method = RequestMethod.POST)
     @ResponseBody
     public ReturnResult getOrderByFromUserId(@ApiParam @RequestBody(required = false) Order order) {
