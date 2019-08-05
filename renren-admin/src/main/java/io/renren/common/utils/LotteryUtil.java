7221f2082d1c80086960f9a168443d9d58442ff5
richard <richard.liu@kpmg.com>
2019-08-05 17:34:42 +0800

    新增集赞模块

--- renren-admin/src/main/java/io/renren/common/utils/LotteryUtil.java
+++ renren-admin/src/main/java/io/renren/common/utils/LotteryUtil.java
@@ -17,24 +17,24 @@ public class LotteryUtil {
      * @param orignalRates 原始的概率列表，保证顺序和实际物品对应
      * @return 物品的索引
      */
-    public static int lottery(List<Double> orignalRates) {
+    public static Gift lottery(List<Gift> orignalRates) {
         if (orignalRates == null || orignalRates.isEmpty()) {
-            return -1;
+            return null;
         }
 
         int size = orignalRates.size();
 
         // 计算总概率，这样可以保证不一定总概率是1
         double sumRate = 0d;
-        for (double rate : orignalRates) {
-            sumRate += rate;
+        for (Gift rate : orignalRates) {
+            sumRate += rate.getProbability();
         }
 
         // 计算每个物品在总概率的基础下的概率情况
         List<Double> sortOrignalRates = new ArrayList<Double>(size);
         Double tempSumRate = 0d;
-        for (double rate : orignalRates) {
-            tempSumRate += rate;
+        for (Gift rate : orignalRates) {
+            tempSumRate += rate.getProbability();
             sortOrignalRates.add(tempSumRate / sumRate);
         }
 
@@ -43,36 +43,7 @@ public class LotteryUtil {
         sortOrignalRates.add(nextDouble);
         Collections.sort(sortOrignalRates);
 
-        return sortOrignalRates.indexOf(nextDouble);
-    }
-
-    public static int getJD(List<Double> orignalRates) {
-        if (orignalRates == null || orignalRates.isEmpty()) {
-            return -1;
-        }
-
-        int size = orignalRates.size();
-
-        // 计算总概率，这样可以保证不一定总概率是1
-        double sumRate = 0d;
-        for (double rate : orignalRates) {
-            sumRate += rate;
-        }
-
-        // 计算每个物品在总概率的基础下的概率情况
-        List<Double> sortOrignalRates = new ArrayList<Double>(size);
-        Double tempSumRate = 0d;
-        for (double rate : orignalRates) {
-            tempSumRate += rate;
-            sortOrignalRates.add(tempSumRate / sumRate);
-        }
-
-        // 根据区块值来获取抽取到的物品索引
-        double nextDouble = Math.random();
-        sortOrignalRates.add(nextDouble);
-        Collections.sort(sortOrignalRates);
-
-        return sortOrignalRates.indexOf(nextDouble);
+        return orignalRates.get(sortOrignalRates.indexOf(nextDouble));
     }
 
     public static void main(String[] args) {
@@ -89,28 +60,29 @@ public class LotteryUtil {
 
 
         List<Double> orignalRates = new ArrayList<Double>(gifts.size());
-        for (Gift gift : gifts) {
-            double probability = gift.getProbability();
-            if (probability < 0) {
-                probability = 0;
-            }
-            orignalRates.add(probability);
-        }
-
-        // statistics
-        Map<Integer, Integer> count = new HashMap<Integer, Integer>();
-        double num = 10000000;
-       //for (int i = 0; i < num; i++) {
-            int orignalIndex = LotteryUtil.lottery(orignalRates);
-
-            Integer value = count.get(orignalIndex);
-            count.put(orignalIndex, value == null ? 1 : value + 1);
-        //}
-
-        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
-            System.out.println(gifts.get(entry.getKey()) + ", count=" + entry.getValue() + ", probability="
-                    + entry.getValue() / num);
-        }
+        System.out.println(LotteryUtil.lottery(gifts));
+//        for (Gift gift : gifts) {
+//            double probability = gift.getProbability();
+//            if (probability < 0) {
+//                probability = 0;
+//            }
+//            orignalRates.add(probability);
+//        }
+//
+//        // statistics
+//        Map<Integer, Integer> count = new HashMap<Integer, Integer>();
+//        double num = 10000000;
+//       //for (int i = 0; i < num; i++) {
+//            int orignalIndex = LotteryUtil.lottery(orignalRates);
+//
+//            Integer value = count.get(orignalIndex);
+//            count.put(orignalIndex, value == null ? 1 : value + 1);
+//        //}
+//
+//        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+//            System.out.println(gifts.get(entry.getKey()) + ", count=" + entry.getValue() + ", probability="
+//                    + entry.getValue() / num);
+//        }
     }
 
 }
